2d73ae297d1cd3a617b45cd8c5d4854465ba31de
275554701@qq.com <275554701@qq.com>
2019-05-23 21:49:43 +0800

    创建项目

---
+++ src/main/java/com/leyou/shop/dao/SkusDao.java
@@ -0,0 +1,8 @@
+package com.leyou.shop.dao;
+
+import com.leyou.shop.model.Skus;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface SkusDao extends Mapper<Skus> {
+
+}
---
+++ src/main/java/com/leyou/shop/model/Skus.java
@@ -0,0 +1,4 @@
+package com.leyou.shop.model;
+
+public class Skus {
+}
